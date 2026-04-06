a4f5b4621c54529e987e2c2d6f9fad3b93996870
ardhian26 <dwiardhi123@gmail.com>
2026-04-06 14:26:38 +0700

    update kode log deteksi dan statisk lewat

--- project-api/absensi/models.py
+++ project-api/absensi/models.py
@@ -3,18 +3,24 @@ from django.db import models
 class DataAnggota(models.Model):
     nama = models.CharField(max_length=100)
     nim = models.CharField(max_length=50)
+    foto_wajah = models.ImageField(upload_to='foto_anggota/', null=True, blank=True)
     encoding_wajah = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        managed = False
+        managed = True 
         db_table = 'data_anggota'
 
     def __str__(self):
         return f"{self.nama} ({self.nim})"
 
 class LogDeteksi(models.Model):
-    id_anggota = models.ForeignKey(DataAnggota, on_delete=models.CASCADE, null=True)
+    id_anggota = models.ForeignKey(
+        DataAnggota, 
+        on_delete=models.CASCADE, 
+        null=True,
+        db_column='id_anggota'  # tambahkan ini
+    )
     waktu_deteksi = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=20)
 
@@ -23,10 +29,21 @@ class LogDeteksi(models.Model):
         db_table = 'log_deteksi'
 
 class StatistikLewat(models.Model):
-    id_anggota = models.ForeignKey(DataAnggota, on_delete=models.CASCADE, null=True)
+    id_anggota = models.ForeignKey(
+        DataAnggota, 
+        on_delete=models.CASCADE, 
+        null=True,
+        db_column='id_anggota'  # tambahkan ini
+    )
     tanggal = models.DateField()
     jumlah_lewat = models.IntegerField(default=0)
 
     class Meta:
         managed = False
         db_table = 'statistik_lewat'
+    
+class SensorStatus(models.Model):
+    last_ping = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = 'sensor_status'
